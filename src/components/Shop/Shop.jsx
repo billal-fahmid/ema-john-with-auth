b5bf74805ae9645f4a72ae7df6e712f4f675cb8d
billal-fahmid <billalcooom@gmail.com>
2023-03-28 18:31:15 +0600

    retrive data from local storage in cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -11,16 +11,33 @@ const Shop = () => {
             .then(data=>setProducts(data))
             // console.log(products)
     },[]);
-
+    
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart)
-    },[])
+        const saveCart = [];
+ 
+        //step 1 get id
+        for(const id in storedCart){
+            // step 2 get product by using the id
+            const addedProduct = products.find(product =>product.id === id )
+            // step 3 get the quantity of the product 
+           if(addedProduct){
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity
+            // console.log(addedProduct,quantity)
+            // step 4 add the addedProduct to the saveCart
+            saveCart.push(addedProduct);
+           }
+           
+        }
+        // step 5 set the cart 
+        setCart(saveCart)
+    },[products])
 
     const [cart , setCart] = useState([])
     // console.log(cart)
     const handleAddToCart=(product)=>{
-        console.log(product)
+        // console.log(product)
         const newCart =[...cart,product];
         setCart(newCart);
         addToDb(product.id)
