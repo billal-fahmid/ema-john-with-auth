35d64b581381adb39ee4f18120e5d32866f9bfdf
billal-fahmid <billalcooom@gmail.com>
2023-04-17 16:17:37 +0600

    update

--- src/components/Cheakout/Cheakout.jsx
+++ src/components/Cheakout/Cheakout.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Cheakout = () => {
     return (
         <div>
-            <h2> this is check out page</h2>
+            <h2> this is check out page !!!!!!!!!!!1</h2>
         </div>
     );
 };
